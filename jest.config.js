f87562ea3a5b5f85a9de92ca7139af71e8701acf
George <gnm7208@gmail.com>
2025-10-17 09:19:17 +0300

    Add root jest.config.js and @babel/core to support direct Jest runs (CodeGrade)

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@testing-library|@adobe/css-tools)/)'
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+};
